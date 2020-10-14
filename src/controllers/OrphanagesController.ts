fd6ae543b61cf685cc9c6f5d32b2d671406b9cc2
ericsonmoreira <moreira.ericson@gmail.com>
2020-10-14 13:27:18 -0300

    finish backend

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -2,6 +2,23 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import { Orphanage } from '../database/entities/Orphanage';
 import orphanageView from '../views/orphanage_view';
+import * as Yup from 'yup';
+
+// schema validation on Orphanage
+const schema = Yup.object().shape({
+  name: Yup.string().required(),
+  latitude: Yup.number().required(),
+  longitude: Yup.number().required(),
+  about: Yup.string().required().max(300),
+  instructions: Yup.string().required(),
+  opening_hours: Yup.string().required(),
+  open_on_week: Yup.boolean().required(),
+  images: Yup.array(
+    Yup.object().shape({
+      path: Yup.string().required(),
+    })
+  ),
+});
 
 export default {
   async index(request: Request, response: Response) {
@@ -15,23 +32,48 @@ export default {
     const { id } = request.params;
 
     const repository = getRepository(Orphanage);
-    
+
     const orphanage = await repository.findOneOrFail(id, {
       relations: ['images'],
     });
-    
+
     return response.json(orphanageView.render(orphanage));
   },
   async create(request: Request, response: Response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_week,
+    } = request.body;
+
+    const repository = getRepository(Orphanage);
+
     const resquestImages = request.files as Express.Multer.File[];
 
     const images = resquestImages.map((image) => {
       return { path: image.filename };
     });
 
-    const repository = getRepository(Orphanage);
+    const data = {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_week,
+      images,
+    };
 
-    const orphanage = repository.create({ ...request.body, images });
+    await schema.validate(data, {
+      abortEarly: false,
+    });
+
+    const orphanage = repository.create(data);
 
     await repository.save(orphanage);
 
--- src/errors/handler.ts
+++ src/errors/handler.ts
@@ -1,7 +1,23 @@
 import { ErrorRequestHandler } from 'express';
+import { ValidationError } from 'yup';
+
+interface IValidationErrors {
+  [key: string]: string[];
+}
 
 const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
-  console.error(error);
+  if (error instanceof ValidationError) {
+    const errors: IValidationErrors = {};
+
+    error.inner.forEach((err) => {
+      errors[err.path] = err.errors;
+    });
+    // bad request
+    return response.status(400).json({ message: 'Validation fail', errors });
+  }
+
+  console.error('error', error);
+  // internal error
   return response.status(500).json({ message: 'Internal server error' });
 };
 
